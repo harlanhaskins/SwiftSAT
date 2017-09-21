2ae30b0ff8cce326182655df27b965075be7bedc
Harlan Haskins <harlan@harlanhaskins.com>
2017-09-21 16:07:02 -0400

    Initial Commit

---
+++ SAT/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SAT
+//
+//  Created by Harlan Haskins on 9/21/17.
+//  Copyright © 2017 Harlan Haskins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
